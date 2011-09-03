92363f38e48d5aaf2d9fd14ffcc9dad6d309269c
emou <emils@fmi.uni-sofia.bg>
2011-09-03 23:23:09 +0300

    Improve runtests.py
    * Add support to pass regular expression patterns when specifying which
    tests to run.
    * Propagate output from the running of the tests. Problem was stderr was
    swallowed, because I didn't realize the TextTestRunner sends its output
    on stderr under normal circumstances.

--- runtests.py
+++ runtests.py
@@ -5,6 +5,7 @@ executes them.
 import argparse
 import logging
 import os
+import re
 import subprocess
 import sys
 
@@ -17,6 +18,15 @@ class TestsExecutor(object):
 
     def __init__(self, args):
         self.args = args
+    
+    def _match_test(self, test_path, patterns):
+        """
+        Tries to match `test_path` of test module against `patterns`, which
+        must've been passed on the command line.
+        """
+        return test_path in patterns or \
+               any(test_path.endswith(p) for p in patterns) or \
+               any(re.match(p, test_path) for p in patterns)
 
     def run(self):
         logger = logging.getLogger(_LOGGER_NAME)
@@ -28,26 +38,36 @@ class TestsExecutor(object):
                            ' directory of the project.')
         os.environ['PYTHONPATH'] = os.path.dirname(os.path.abspath(__file__))
 
+        tests = []
+        for (root, dirs, files) in os.walk(os.getcwd()):
+            for f in files:
+                if f.endswith(_TEST_SUFFIX):
+                    tests.append(os.path.join(root, f))
         if self.args.tests:
-            tests = self.args.tests
-        else:
-            tests = []
-            for (root, dirs, files) in os.walk(os.getcwd()):
-                for f in files:
-                    if f.endswith(_TEST_SUFFIX):
-                        tests.append(os.path.join(root, f))
+            tests = filter(
+                lambda t: self._match_test(t, self.args.tests),
+                tests
+            )
 
         for f in tests:
-                logger.info('Running test %s' % f)
-                ret = subprocess.call([sys.executable, '-tt', f])
-                if ret != 0:
-                    logger.info('\n====== TESTS FAILED. ======')
-                    return ret
+            logger.info('Running %s' % f)
+            p = subprocess.Popen([sys.executable, '-tt', f],
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            (out, err) = p.communicate()
+            if out:
+                logger.info(out)
+            if err:
+                # Regular test output from the TextTestRunner goes to stderr,
+                # but this is info for us.
+                logger.info(err)
+            if p.returncode != 0:
+                return p.returncode
         return 0
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run the Duck tests.')
+    parser = argparse.ArgumentParser(description='Run the Duck tests. Stop on first failed test module.')
     parser.add_argument('tests', metavar='T', type=str, nargs='*',
-                        help='Run only the specified tests')
+                        help='Run only the specified tests. You can specify regular expression patterns.')
     args = parser.parse_args()
     sys.exit(TestsExecutor(args).run())
