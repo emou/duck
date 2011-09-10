58037bfcbc630e3c586b889665276e74f3beab39
Emil Stanchev <stanchev.emil@gmail.com>
2011-09-10 17:35:09 +0000

    Better error handling. Don't show traceback by default. Should add a --debug option later on.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 import sys
-from os.path import dirname, abspath
-
-sys.path.append(dirname(abspath(__file__)))
-from duck.frontend.wxwidgets import Frontend
-from duck.backend.mpd import Backend
-from duck.errors import FatalError
+from os.path import dirname, abspath, split
 
 def main(argv):
-    try:
-        return Frontend(Backend).run()
-    except FatalError:
-        sys.exit(111)
+   sys.path.append(dirname(abspath(__file__)))
+   from duck.frontend.wxwidgets import Frontend
+   from duck.backend.mpd import Backend
+   from duck.errors import FatalError
+   return Frontend(Backend).run()
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    try:
+       sys.exit(main(sys.argv))
+    except Exception, e:
+       print 'duck: %s' % e
+       try:
+          errorcode = e.errcode
+       except AttributeError:
+          errorcode = 1
+       sys.exit(errorcode)
